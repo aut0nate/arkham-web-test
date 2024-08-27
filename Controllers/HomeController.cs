703aabb18225ea64e03acfbbae109343fc8f89a6
Nathan Darker <n.darker@arkham.live>
2024-08-27 15:13:58 +0100

    Created new contact page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,30 +2,49 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ArkhamUK.Models;
 
-namespace ArkhamUK.Controllers;
-
-public class HomeController : Controller
+namespace ArkhamUK.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // In a real application, you would save the contact message to the database or send an email.
+                ViewBag.Message = "Thank you for contacting us!";
+                return View();
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,10 @@
+namespace ArkhamUK.Models
+{
+    public class ContactViewModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+    }
+}
+
